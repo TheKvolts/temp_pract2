0a262db8e3718870831257229a33018579cc2c47
Kalen <kalentru@seas.upenn.edu>
2024-05-03 15:24:30 -0400

    moved bitbox outside of docs

--- bitbox/coordination.py
+++ bitbox/coordination.py
@@ -1,13 +1,12 @@
 def cross_correlation(data1, data2):
-    
-    """Computes the cross correlation between two datasets.
+    """Computess the cross correlation between two datasets.
 
-    param data1: dataset 1 description
-    type data1: JSON?
-    param data2: dataset 2 description
-    type data2: JSON?
-    return: cross correlation value
-    rtype: float
+    :param data1: dataset 1 description
+    :type data1: JSON?
+    :param data2: dataset 2 description
+    :type data2: JSON?
+    :return: cross correlation value
+    :rtype: float
     """
 
     print("computing cross correlation")
