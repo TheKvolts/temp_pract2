a37c9a92dab280d5dcbe544203aefc1b6ba38a00
Kalen <kalentru@seas.upenn.edu>
2024-05-03 15:28:45 -0400

    updated conf pointer, created src folder

--- src/bitbox/coordination.py
+++ src/bitbox/coordination.py
@@ -1,11 +1,11 @@
 def cross_correlation(data1, data2):
-    """Computess the cross correlation between two datasets.
+    """Computes the cross correlation between two datasets.
 
     :param data1: dataset 1 description
     :type data1: JSON?
     :param data2: dataset 2 description
     :type data2: JSON?
-    :return: cross correlation value
+    :return: cross correlation
     :rtype: float
     """
 
