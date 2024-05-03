d5a3df5277ef2cb3caafd1194646e0378a7303da
Kalen <kalentru@seas.upenn.edu>
2024-05-03 13:35:39 -0400

    added og bitbox and put in bitboxrst into docs

--- bitbox/expressions.py
+++ bitbox/expressions.py
@@ -1,23 +1,8 @@
 def symmetry(landmarks):
-    """Calculate the symmetry of the face
-
-    :param landmarks: Facial landmarks
-    :type landmarks: _type_
-    """
     print("Calculating symmetry")
 
 def expressivity(landmarks):
-    """ Calculate the expressivity of the face
-
-    :param landmarks: Facial landmarks
-    :type landmarks: _type_
-    """
     print("Calculating expressivity")
 
 def diversity(landmarks):
-    """ Calculate the diversity of the face
-
-    :param landmarks: Facial landmarks
-    :type landmarks: _type_
-    """
     print("Calculating diversity")
--- bitbox/kinematics.py
+++ bitbox/kinematics.py
@@ -1,23 +1,8 @@
 def spatial_extent(landmarks):
-    """ Determines the spatial extent of the face
-
-    :param landmarks: _description_
-    :type landmarks: _type_
-    """
     print("Calculating spatial extent")
 
 def speed(landmarks):
-    """ Calculate the speed of the face
-
-    :param landmarks: _description_
-    :type landmarks: _type_
-    """
     print("Calculating speed")
 
 def smoothness(landmarks):
-    """ Calculate the smoothness of the face
-
-    :param landmarks: _description_
-    :type landmarks: _type_
-    """
     print("Calculating smoothness")
